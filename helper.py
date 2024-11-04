0d25fb9358f2079844c4b53c06a7059c7b25a73d
AndrMoura <andre_ptos29@msn.com>
2024-11-04 13:35:53 +0000

    add TTS wip

--- helper.py
+++ helper.py
@@ -1,59 +1,52 @@
 import base64
 import time
 import streamlit as st
+import tempfile
+import soundfile as sf
 
-from mutagen.mp3 import MP3
-from langchain.llms.ollama import Ollama
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
-from prompt import question_generator_prompt
-from gtts import gTTS
-
-
-def generate_questions(resume, role, role_description, model_name="llama3.2:1b"):
-
-    question_generator_template = PromptTemplate(
-        template=question_generator_prompt,
-        input_variables=["role", "description", "resume_text"],
-    )
-
-    llm = Ollama(model=model_name)
-
-    question_generator_chain = LLMChain(llm=llm, prompt=question_generator_template)
-
-    guidelines = question_generator_chain.run(
-        role=role, description=role_description, resume_text=resume
-    )
-
-    return guidelines
+# from mutagen.mp3 import MP3
+# from langchain.llms.ollama import Ollama
+# from langchain.chains import LLMChain
+# from langchain.prompts import PromptTemplate
+# from prompt import question_generator_prompt
+# from gtts import gTTS
 
 
-def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
+# def generate_questions(resume, role, role_description, model_name="llama3.2:1b"):
+
+#     question_generator_template = PromptTemplate(
+#         template=question_generator_prompt,
+#         input_variables=["role", "description", "resume_text"],
+#     )
+
+#     llm = Ollama(model=model_name)
+
+#     question_generator_chain = LLMChain(llm=llm, prompt=question_generator_template)
+
+#     guidelines = question_generator_chain.run(
+#         role=role, description=role_description, resume_text=resume
+#     )
+
+#     return guidelines
+
+
+def autoplay_audio_from_array(audio_array, sample_rate, duration):
+
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio_file:
+        # Save numpy array as a wav file
+        sf.write(temp_audio_file.name, audio_array, sample_rate)
+
+        # Read the temp file in binary mode to convert to base64
+        with open(temp_audio_file.name, "rb") as f:
+            data = f.read()
+            b64 = base64.b64encode(data).decode()
+
+        # Streamlit HTML to autoplay the audio
         placeholder = st.empty()
-        md = f"""
-            <audio id='audio' autoplay="true" style="display: none">
-            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+        audio_html = f"""
+            <audio autoplay="true" style="display:none;">
+                <source src="data:audio/wav;base64,{b64}" type="audio/wav">
             </audio>
-            """
-        placeholder.markdown(
-            md,
-            unsafe_allow_html=True,
-        )
-
-
-def play_audio(response, file_path):
-    tts = gTTS(response)
-    tts.save(file_path)
-    autoplay_audio(file_path)
-
-
-def play_audio_block(response, file_path):
-    tts = gTTS(response)
-    tts.save(file_path)
-    audio = MP3(file_path)
-    duration = audio.info.length
-    autoplay_audio(file_path)
-    time.sleep(duration)
+        """
+        placeholder.markdown(audio_html, unsafe_allow_html=True)
+        time.sleep(duration)
--- pages/interview_test.py
+++ pages/interview_test.py
@@ -1,17 +1,32 @@
 import streamlit as st
 import datetime
-from helper import play_audio, play_audio_block
-from streamlit_mic_recorder import mic_recorder
-from audio_recorder_streamlit import audio_recorder
-from audiorecorder import audiorecorder
-from pydub import AudioSegment
-from audio import AudioToText
+import tempfile
+import base64
+import soundfile as sf
 
-audio_path = "answer_temp.wav"
+from audiorecorder import audiorecorder
+from audio import AudioToText, TextToAudio
+from helper import autoplay_audio_from_array
+
 
 st.title("Interview in Progress 🎯")
 
-audio_to_text = AudioToText("small")
+
+@st.cache_resource
+def load_TTS(model_name):
+    return AudioToText(model_name)
+
+
+@st.cache_resource
+def load_STT(model_name):
+    return TextToAudio(model_name=model_name, device="cuda:0")
+
+
+tts = load_TTS("small")
+stt = load_STT("hf://SWivid/F5-TTS/F5TTS_Base/model_1200000.safetensors")
+
+audio_path = "answer_temp.wav"
+audio_ref_path = "sample.wav"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -23,57 +38,46 @@ def get_now_date():
 
 def direct_text_prompt():
     """Render and return the user's input."""
-    placeholder = f"Answer your interviewer"
 
     with st.container():
-        left, right = st.columns([300, 145])
-        with left:
-            text_from_chat_input_widget = st.chat_input(placeholder=placeholder)
-        with right:
-            text_audio_recorder = widget_mic_recorder()
-            # saved_path = audio_to_text.save_audio(text_audio_recorder, audio_path)
-            # text_audio = audio_to_text.transcribe_audio(saved_path)
-            text_audio = "ok"
+        left, right = st.columns([0.2, 0.8])
+        with st.spinner(""):
+            with left:
+                pass
+            with right:
+                text_audio_recorder = audiorecorder("", "", True, "audio_recorder")
+                saved_path = tts.save_audio(text_audio_recorder, audio_path)
+                text_audio = tts.transcribe_audio(saved_path)
 
-    return text_from_chat_input_widget or text_audio
+    return text_audio
 
 
-def widget_mic_recorder() -> AudioSegment:
-    """Record audio from the microphone."""
-    red_square = "\U0001F7E5"
-    microphone = "\U0001F3A4"
-    play_button = "\U000025B6"
+def display_message(role, content):
+    """Display a chat message for a given role ('user' or 'assistant') with a timestamp."""
+    time_now = get_now_date()
+    with st.chat_message(role):
+        st.caption(time_now)
+        st.markdown(content)
 
-    recording = mic_recorder(
-        key=f"audiorecorder_widget_",
-        start_prompt=play_button + microphone,
-        stop_prompt=red_square,
-        just_once=True,
-        use_container_width=True,
-        format="wav",
+    st.session_state.messages.append(
+        {"role": role, "timestamp": time_now, "content": content}
     )
-    if recording is None:
-        return AudioSegment.silent(duration=0)
-
-    return AudioSegment(
-        data=recording["bytes"],
-        sample_width=recording["sample_width"],
-        frame_rate=recording["sample_rate"],
-        channels=1,
-    )
-
-    # audio = audiorecorder("", "", True, "test_key"), 
 
 
 chat_msgs_container = st.container(height=550, border=False)
 with chat_msgs_container:
     with st.chat_message("assistant"):
+        intro_text = 'greetings sons of bitches'
         st.caption(get_now_date())
-        st.markdown("greetings sons of bitches")
+        st.markdown(intro_text)
+        
+        response = stt.generate_audio_response(
+            ai_response=intro_text,
+            ref_audio=audio_ref_path,
+            ref_text="",
+            remove_silence=True,
+        )
 
-
-with chat_msgs_container:
-    # Display chat messages from history on app rerun
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.caption(message["timestamp"])
@@ -81,49 +85,19 @@ with chat_msgs_container:
 
 
 prompt = direct_text_prompt()
+
 with chat_msgs_container:
     if prompt:
-        time_now = get_now_date()
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.caption(time_now)
-            st.markdown(prompt)
-            st.session_state.messages.append(
-                {"role": "user", "timestamp": time_now, "content": prompt}
-            )
+        display_message("user", prompt)
 
-        with st.chat_message("assistant"):
-            test_res = prompt
-            time_now = get_now_date()
-            text_reply_container = st.empty()
-            text_reply_container.caption(time_now)
-            text_reply_container.markdown(test_res)
-            st.session_state.messages.append(
-                {"role": "assistant", "timestamp": time_now, "content": test_res}
-            )
-
-# if prompt := st.chat_input(""):
-#     st.chat_message("user").markdown(prompt)
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-
-#     response = f"Echo: {prompt}"
-#     # play_audio(response, temporary_audio)
-#     with st.chat_message("assistant"):
-#         st.markdown(response)
-
-#     st.session_state.messages.append({"role": "assistant", "content": response})
-
-
-# if not st.session_state.get("ongoing_interview"):
-#     init_question = "Hello and welcome"
-
-#     with st.chat_message("assistant"):
-#         st.caption("test")
-#         st.markdown(init_question)
-#         st.session_state.ongoing_interview = True
-
-#     play_audio_block(init_question, temporary_audio)
-#     st.session_state.messages.append({"role": "assistant", "content": init_question})
-
-
-# audio_bytes = audio_recorder(auto_start=True, pause_threshold=1)
+        response = stt.generate_audio_response(
+            ai_response=prompt,
+            ref_audio=audio_ref_path,
+            ref_text="",
+            remove_silence=True,
+        )
+        print("response", response)
+        autoplay_audio_from_array(
+            response["wave_data"], response["sample_rate"], response["duration"]
+        )
+        display_message("assistant", prompt)
